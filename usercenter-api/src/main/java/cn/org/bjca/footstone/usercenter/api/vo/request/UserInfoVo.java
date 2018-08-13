25e44b4aa531d15ce6be1c1ac6773bfbc55075a5
shuwei <shuwei@bjca.org.cn>
2018-08-13 18:04:35 +0800

    实名认证VO

---
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/vo/request/UserInfoVo.java
@@ -0,0 +1,34 @@
+package cn.org.bjca.footstone.usercenter.api.vo.request;
+
+import lombok.Data;
+
+/**
+ * @author shuvigoss@gmail.com (ShuWei) 2018/8/13
+ * @since 1.0
+ */
+@Data
+public class UserInfoVo {
+
+  private String headImgUrl;
+
+  private String name;
+
+  private String idType;
+
+  private String idNum;
+  
+  private String mobile;
+
+  private String email;
+
+  private String bankCardNum;
+
+  private String faceIdImageUrl;
+
+  private String idCardFrontImageUrl;
+
+  private String idCardBackImageUrl;
+
+  private String fromPlatform;
+
+}
---
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/vo/response/UserInfoResponse.java
@@ -0,0 +1,13 @@
+package cn.org.bjca.footstone.usercenter.api.vo.response;
+
+import lombok.Data;
+
+/**
+ * @author shuvigoss@gmail.com (ShuWei) 2018/8/13
+ * @since 1.0
+ */
+@Data
+public class UserInfoResponse {
+
+  private Integer uid;
+}
