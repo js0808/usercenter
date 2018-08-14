daae3a08380c4a7b806f5068c20ca69a88139ef1
zhaozhiwei <zhaozhiwei@bjca.org.cn>
2018-08-14 17:30:11 +0800

    实名方式

--- usercenter-server/src/main/java/cn/org/bjca/footstone/usercenter/biz/EntInfoService.java
+++ usercenter-server/src/main/java/cn/org/bjca/footstone/usercenter/biz/EntInfoService.java
@@ -1,9 +1,12 @@
 package cn.org.bjca.footstone.usercenter.biz;
 
+import cn.org.bjca.footstone.usercenter.api.enmus.RealNameTypeEnum;
 import cn.org.bjca.footstone.usercenter.api.vo.request.EntInfoCheakRequest;
 import cn.org.bjca.footstone.usercenter.api.vo.request.EntInfoRequest;
 import cn.org.bjca.footstone.usercenter.dao.mapper.EntInfoMapper;
+import cn.org.bjca.footstone.usercenter.dao.model.EntInfo;
 import cn.org.bjca.footstone.usercenter.exceptions.BaseException;
+import jodd.bean.BeanCopy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,9 +38,23 @@ public class EntInfoService {
         .isBlank(socialCreditCode)) {
       throw new BaseException("");
     }
-
+    //目前只支持:ent_base,企业基本信息认证
     String realNameType = entInfoCheakRequest.getRealNameType();
+    if (!StringUtils.equals(realNameType, RealNameTypeEnum.ENT_BASE.getDesc())) {
+
+    }
+    boolean real = checkRealRemote();
+    if (real) {
+      EntInfo entInfo = new EntInfo();
+      BeanCopy.beans(entInfoCheakRequest, entInfo).copy();
+      entInfoMapper.insertSelective(entInfo);
+    } else {
+      throw new BaseException("");
+    }
+  }
 
 
+  private boolean checkRealRemote() {
+    return true;
   }
 }
