7b230c30f3a0da3246aae474f972ef4a69a10cab
zhaozhiwei <zhaozhiwei@bjca.org.cn>
2018-08-15 14:29:00 +0800

    error code

--- usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/enmus/ReturnCodeEnum.java
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/enmus/ReturnCodeEnum.java
@@ -8,66 +8,69 @@ package cn.org.bjca.footstone.usercenter.api.enmus;
  **/
 public enum ReturnCodeEnum {
 
-    SUCCESS(200, "成功"),
+  SUCCESS(200, "成功"),
 
-    REQ_PARAM_ERR(60604001, "请求参数错误"),
+  REQ_PARAM_ERR(60604001, "请求参数错误"),
 
-    SQL_EXCEPTION(60604002, "操作数据库异常"),
+  SQL_EXCEPTION(60604002, "操作数据库异常"),
 
-    APPID_NOT_EXIST(60604003, "接入方ID不存在"),
+  APPID_NOT_EXIST(60604003, "接入方ID不存在"),
 
-    APP_INFO_PUBKEY_ERROR(60604004, "获得接入方公钥错误"),
+  APP_INFO_PUBKEY_ERROR(60604004, "获得接入方公钥错误"),
 
-    SIGN_VERIFY_ERROR(60604005, "验证请求签名错误"),
+  SIGN_VERIFY_ERROR(60604005, "验证请求签名错误"),
 
-    ERROR(60609999, "服务端异常"),
-    REALNAME_TYPE_ERROR(60604006,"错误的实名认证方式"),
+  ERROR(60609999, "服务端异常"),
 
-    REALNAME_NOT_EXIST(60604007,"未找到实名认证信息uid {0}"),
+  REALNAME_TYPE_ERROR(60604006, "错误的实名认证方式"),
 
-    REALNAME_PARAM_ERROR(60604008,"实名认证参数错误 {0}"),
+  REALNAME_NOT_EXIST(60604007, "未找到实名认证信息uid {0}"),
 
-    ID_SERVICE_ERROR(60604009,"身份核实错误[%s]"),
+  REALNAME_PARAM_ERROR(60604008, "实名认证参数错误 {0}"),
 
-    ID_SERVICE_CONN_ERROR(60604010,"身份核实服务通信异常"),
+  ID_SERVICE_ERROR(60604009, "身份核实错误[%s]"),
 
-    MSG_SERVER_ERROR(60604011,"短信消息服务异常: %s"),
+  ID_SERVICE_CONN_ERROR(60604010, "身份核实服务通信异常"),
 
-    AUTH_CODE_NOT_EXIT_ERROR(60604012, "验证码不存在或已过期"),
+  MSG_SERVER_ERROR(60604011, "短信消息服务异常: %s"),
 
-    AUTH_CODE_VALIDATE_ERROR(60604013, "验证码验证失败");
+  AUTH_CODE_NOT_EXIT_ERROR(60604012, "验证码不存在或已过期"),
 
-    /**
-     * The value.
-     */
-    private final int value;
+  AUTH_CODE_VALIDATE_ERROR(60604013, "验证码验证失败"),
 
-    /**
-     * The desc.
-     */
-    private final String desc;
+  ENT_INFO_NOT_ENOUGH(60604012, "统一社会信用代码/工商营业执照号/组织机构号不能同时为空");
 
-    ReturnCodeEnum(final int value, final String desc) {
-        this.value = value;
-        this.desc = desc;
-    }
+  /**
+   * The value.
+   */
+  private final int value;
 
-    /**
-     * Gets the value.
-     *
-     * @return the value
-     */
-    public int value() {
-        return this.value;
-    }
+  /**
+   * The desc.
+   */
+  private final String desc;
 
-    /**
-     * Gets the desc.
-     *
-     * @return the desc
-     */
-    public String getDesc() {
-        return this.desc;
-    }
+  ReturnCodeEnum(final int value, final String desc) {
+    this.value = value;
+    this.desc = desc;
+  }
+
+  /**
+   * Gets the value.
+   *
+   * @return the value
+   */
+  public int value() {
+    return this.value;
+  }
+
+  /**
+   * Gets the desc.
+   *
+   * @return the desc
+   */
+  public String getDesc() {
+    return this.desc;
+  }
 
 }
