4fe0e8b46884b9c3d52308c2b9e189a67cd13b3b
shuwei <shuwei@bjca.org.cn>
2018-08-17 13:10:04 +0800

    增加通过账号查询企业及个人接口

---
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/enmus/UserTypeEnum.java
@@ -0,0 +1,11 @@
+package cn.org.bjca.footstone.usercenter.api.enmus;
+
+/**
+ * @author shuvigoss@gmail.com (ShuWei) 2018/8/17
+ * @since 1.0
+ */
+public enum UserTypeEnum {
+
+  USER, //个人
+  ENT   //企业
+}
---
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/vo/request/EntInfoQueryRequest.java
@@ -0,0 +1,14 @@
+package cn.org.bjca.footstone.usercenter.api.vo.request;
+
+import lombok.Data;
+
+/**
+ * @author shuvigoss@gmail.com (ShuWei) 2018/8/17
+ * @since 1.0
+ */
+@Data
+public class EntInfoQueryRequest {
+
+  private String account;
+
+}
