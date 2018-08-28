6d7ebea7e405d9cf1d746abe8cbd470256af2642
baoqingbin <baoqingbin@wanda.cn>
2018-08-28 14:01:23 +0800

    appId

---
+++ usercenter-api/src/main/java/cn/org/bjca/footstone/usercenter/api/vo/request/BaseRequest.java
@@ -0,0 +1,15 @@
+package cn.org.bjca.footstone.usercenter.api.vo.request;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+@Data
+public class BaseRequest {
+
+  @ApiModelProperty(value = "appId")
+  @NotBlank
+  @Length(max = 64)
+  private String appId;
+}
