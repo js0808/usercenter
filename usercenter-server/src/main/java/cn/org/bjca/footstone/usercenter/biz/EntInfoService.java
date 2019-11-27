5437554f2d849b44339a334d9c0dfc688a006aea
libiao <libiao@bjca.org.cn>
2019-11-27 09:51:24 +0800

    只对多条已实名认证记录做校验。

--- usercenter-server/src/main/java/cn/org/bjca/footstone/usercenter/biz/EntInfoService.java
+++ usercenter-server/src/main/java/cn/org/bjca/footstone/usercenter/biz/EntInfoService.java
@@ -42,6 +42,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 import jodd.bean.BeanCopy;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
@@ -383,12 +384,23 @@ public class EntInfoService {
       return null;
     }
 
-    if (CollectionUtils.size(entInfos) > 1) {
-      log.error("查找企业信息，发现多条记录。socialCreditCode:{},oldName:{}", socialCreditCode, oldName);
+    /**
+     * 已实名企业
+     */
+    final List<EntInfo> realNames = entInfos.stream().filter(entInfo -> Objects.nonNull(entInfo.getRealNameFlag()))
+        .filter(entInfo -> entInfo.getRealNameFlag().intValue() == 1)
+        .collect(Collectors.toList());
+    final int realNameSize = CollectionUtils.size(realNames);
+    if (realNameSize > 1) {
+      log.error("查找企业信息，发现多条已实名记录。socialCreditCode:{},oldName:{}", socialCreditCode, oldName);
       throw new BjcaBizException(ReturnCodeEnum.REALNAME_PARAM_ERROR, "找到匹配的多条记录");
     }
-
-    return entInfos.get(0);
+    if (realNameSize == 1) {
+      //如果恰好有一条实名认证记录，返回
+      return realNames.get(0);
+    }
+    //如果实名记录为空，取最后一条未实名认证的记录返回
+    return entInfos.get(CollectionUtils.size(entInfos) - 1);
   }
 
   /**
